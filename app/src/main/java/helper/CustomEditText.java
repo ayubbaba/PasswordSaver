0a0cb714340a401ce2b36bb743db574d00ca054f
ayubbaba <ayubbabacbz@gmail.com>
2017-01-20 00:36:22 +0530

    version 1.2

--- app/src/main/java/helper/CustomEditText.java
+++ app/src/main/java/helper/CustomEditText.java
@@ -3,13 +3,11 @@ package helper;
 import android.content.Context;
 import android.util.AttributeSet;
 
-import com.rengwuxian.materialedittext.MaterialEditText;
-
 /**
  * Created by ayub on 19-Jan-17.
  */
 
-public class CustomEditText extends MaterialEditText {
+public class CustomEditText extends android.support.v7.widget.AppCompatTextView {
     public CustomEditText(Context context) {
         super(context);
     }
